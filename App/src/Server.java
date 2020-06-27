229a34c8aaf1595e1206443ae64b8c0ad6bf9dd6
FabianCristancho <fabiancrisrincon@gmail.com>
2020-06-27 11:54:56 -0500

    update

--- App/src/Server.java
+++ App/src/Server.java
@@ -8,18 +8,44 @@ public class Server extends HttpServlet {
    private String message;
 
    public void init() throws ServletException {
-      // Do required initialization
       message = "Hacer ping a server2";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
       
-      // Set response content type
+      
       response.setContentType("text/html");
-// Actual logic goes here.
+
       PrintWriter out = response.getWriter();
       out.println("<a href='/App/ping'>" + message + "</a>");
+
+
+
+      try {
+
+		Process p = Runtime.getRuntime().exec("ping 192.168.100.136");
+		out.println("<p>Entra aqui</p>");
+		BufferedReader inputStream = new BufferedReader(
+				new InputStreamReader(p.getInputStream()));
+
+		String s = "";
+		
+		// reading output stream of the command
+		while ((s = inputStream.readLine()) != null) {
+			out.println("<p>"+s+"</p>");
+		}
+		out.println("<p>No entra en while</p>");
+
+	} catch (Exception e) {
+		out.println("<p>"+e.getMessage()+"</p>");
+		
+	}
+
+
+
+
+      out.println("<p>Este es otro parrafo, arriba hay algo</p>");
    }
 
    public void destroy() {
