f43f6e942a86cdc0cbcfb51b9ad705999a50017b
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-06-27 20:18:59 -0500

    logs listos y comienzo de gui

--- App/src/Server.java
+++ App/src/Server.java
@@ -18,7 +18,26 @@ public class Server extends HttpServlet {
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
-      out.println("<a href='/App/ping'>" + message + "</a>");
+      out.println("<!DOCTYPE html>");
+      out.println("<html>");
+         out.println("<head>");
+            out.println("<meta charset='utf-8'>");
+            out.println("<title>Administrador de servidores</title>");
+            out.println("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk' crossorigin='anonymous'>");
+            out.println("<script src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>");
+            out.println("<script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin='anonymous'></script>");
+            out.println("<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js' integrity='sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI' crossorigin='anonymous'></script>");
+         out.println("</head>");
+
+         out.println("<body>");
+            out.println("<div class='container'>");
+               out.println("<h1 class='text-center text-primary'>ADMINISTRADOR DE SERVIDORES</h1>");
+               out.println("<a href='/App/ping'>" + message + "</a>");
+               out.println("<a href='#' class='btn btn-primary'> Ping </a>");
+            out.println("</div>");
+         out.println("</body>");
+         
+      out.println("</html>");
    }
 
    public void destroy() {
